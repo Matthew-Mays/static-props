20fd8233913ecde3cbbfe45803feec9e9bea6010
Matthew-Mays <matthew.c.mays96@gmail.com>
2023-01-12 16:36:18 -0600

    add pre-rendering and replace dummy html with backend data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-const HomePage = () => {
+import fs from "fs/promises";
+import path from "path";
+
+const HomePage = (props) => {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+};
+
+export const getStaticProps = async () => {
+  console.log("(Re-)Generating");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 20,
+  };
 };
 
 export default HomePage;
